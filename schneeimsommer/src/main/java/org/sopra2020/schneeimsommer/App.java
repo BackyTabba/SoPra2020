1e4eb9fea060004c176fb685288c200408f14475
BackyTabba <l-lee12@web.de>
2020-03-10 12:10:35 +0100

    snap tools importiert

--- schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/App.java
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/App.java
@@ -1,5 +1,10 @@
 package org.sopra2020.schneeimsommer;
 
+import org.esa.snap.core.dataio.ProductIO;
+import org.esa.snap.core.datamodel.Product;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,15 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        Product product = null;
+        try {
+            product = ProductIO.readProduct("test");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println( "Hello World!" );
+
     }
 }
