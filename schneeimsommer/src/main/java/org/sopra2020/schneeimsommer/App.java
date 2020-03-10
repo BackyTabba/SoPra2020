b598624904d9ae03cfc041c81f02a26990e7438e
jana2308walter <61973873+jana2308walter@users.noreply.github.com>
2020-03-10 12:19:26 +0100

    Update App.java

--- schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/App.java
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/App.java
@@ -4,24 +4,18 @@ import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.datamodel.Product;
 
 import java.io.IOException;
-
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main (String[] args)
     {
-
         Product product = null;
-        try {
+        try
+        {
             product = ProductIO.readProduct("test");
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
-
-        System.out.println( "Hello World!" );
-
     }
 }
