e236db2bdc31ecac1659ed3228373b07289b3498
BackyTabba <l-lee12@web.de>
2020-03-10 11:06:18 +0100

    Maven Projekt erstellt (GithubDesktop)

---
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/App.java
@@ -0,0 +1,13 @@
+package org.sopra2020.schneeimsommer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ schneeimsommer/src/test/java/org/sopra2020/schneeimsommer/AppTest.java
@@ -0,0 +1,20 @@
+package org.sopra2020.schneeimsommer;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
