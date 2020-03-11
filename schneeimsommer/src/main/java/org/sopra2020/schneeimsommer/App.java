527ab7e8c4860387578b8c8314026dd2c580a4ef
BackyTabba <l-lee12@web.de>
2020-03-11 16:26:46 +0100

    App angepasst

--- schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/App.java
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/App.java
@@ -1,19 +1,29 @@
 package org.sopra2020.schneeimsommer;
 
+import com.bc.ceres.glevel.MultiLevelImage;
+import com.google.common.primitives.Floats;
 import org.esa.snap.core.dataio.ProductIO;
 import org.esa.snap.core.datamodel.Product;
-import java.io.IOException;
 
+import java.awt.*;
+import java.awt.image.Raster;
+import java.io.IOException;
 import com.bc.ceres.core.PrintWriterProgressMonitor;
 import org.esa.snap.core.datamodel.Band;
+import org.esa.snap.core.datamodel.ProductData;
+
 import javax.imageio.stream.FileImageOutputStream;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static jdk.nashorn.internal.objects.NativeMath.max;
 
 public class App
 {
-  /*  public static void main (String[] args)
+    /*public static void main (String[] args)
     {
-        String inputPath =
+        String inputPath = "",outputPath="";
         if (args.length < 2)
         {
             System.out.println("parameter usage: <input-file> <output-file>");
@@ -33,9 +43,10 @@ public class App
         {
             System.out.println("error: " + e.getMessage());
         }
+    }
 
         Product product = null;
-        system.out.println ("Baender:" + product.getBandNames());
+        System.out.println ("Baender:" + product.getBandNames());
         try
         {
             product = ProductIO.readProduct("test");
@@ -48,20 +59,20 @@ public class App
 
         public static void main(String[] args)
         {
-           if (args.length < 2)
+         /*  if (args.length < 2)
            {
                 System.out.println("parameter usage: <input-file> <output-file>");
                 return;
-            }
+            }*/
 
             // Get arguments
-            String inputPath = args[0];
-            String outputPath = args[1];
+            String inputPath = "C:\\Users\\Lennart\\Downloads\\S1B_IW_GRDH_1SDV_20200302T171606_20200302T171631_020516_026E05_DC99.zip";
+            String outputPath = "C:\\temp\\test";
 
             try
             {
                 // Pass arguments to actual program code
-                run(inputPath, outputPath);
+                writeData(inputPath, outputPath);
             }
             catch (IOException e)
             {
@@ -69,105 +80,152 @@ public class App
             }
         }
 
-        private static void run(String inputPath, String outputPath)
+        public static void writeData(String inputPath,String outputPath)throws IOException {
+            Product product = ProductIO.readProduct(inputPath);
+            Band band = product.getBand("Amplitude_VH");
+            MultiLevelImage image = band.getGeophysicalImage();
+            Rectangle rechteck = new Rectangle(10550, 7380, 1000, 600);
+            Raster raster = image.getData(rechteck);
+            FileImageOutputStream outputStream = new FileImageOutputStream(new File(outputPath));
+            float[] Pixels = new float[1000];//länge
+
+            //For each pixelreihe
+
+           // float allavg=Pixels[];
+            float allmax=0;
+            float allmin=10000;
+            for (int i = 0; i < raster.getHeight(); i++) {
+
+
+                raster.getPixels(10550, 7380+i, 1000, 1, Pixels);
+              //  outputStream.writeByte(Pixels[i]);
+                outputStream.writeFloat(Pixels[i]);
+                //System.out.println(Arrays.toString(Pixels));#
+
+                if(allmin>Floats.min(Pixels)){
+                    allmin=Floats.min(Pixels);
+                }
+                if(allmax<Floats.max(Pixels)){
+                    allmax=Floats.max(Pixels);
+                }
+                System.out.println(Floats.max(Pixels)+" "+Floats.min(Pixels));
+
+               // Arrays.stream(Pixels).average().orElse(Double.NaN);
+
+            }
+            System.err.println(allmax+"  "+allmin);
+            
+
+        }
+        private float avg(Float[] f){
+            float sum=0;
+        for(float a : f){
+            sum+=a;
+        }
+        return sum/f.length;
+        }
+
+        /*private static void run(String inputPath, String outputPath)
                 throws IOException
         {
-            // Read the product (note that only 'nodes' are read, not the entire data!)
             Product product = ProductIO.readProduct(inputPath);
 
-            // Get the scene width
-            int w = product.getSceneRasterWidth();
+        // Get the "high" band
+        Band hiBand = product.getBand("radiance_10");
 
-            // Get the scene height
-            int h = product.getSceneRasterHeight();
+        if (hiBand == null)
+        {
+            throw new IOException("hi-band 'radiance_10' not found");
+        }
 
             // Get the "low" band
             Band lowBand = product.getBand("radiance_6");
+            int hier = product.getBandNames().length;
+            String hier2 = Arrays.toString(product.getBandNames());
+            product.getSceneRasterHeight();
+            product.getSceneRasterWidth();
 
-            if (lowBand == null)
-            {
-                throw new IOException("low-band 'radiance_6' not found");
-            }
 
             // Get the "high" band
-            Band hiBand = product.getBand("radiance_10");
+            Band hiBand = product.getBand("Amplitude_VH");
+            MultiLevelImage testimage = hiBand.getGeophysicalImage();
+           // ProductData testdaten = hiBand.getData();
+            Rectangle testrechteck = new Rectangle(10550, 7380,1000,600);
+            Raster testraster = hiBand.getGeophysicalImage().getData(testrechteck);
+            int width = testraster.getWidth();
+            int heigth = testraster.getHeight();
+            int minX = testraster.getMinX();
+            int minY = testraster.getMinY();
 
-            if (hiBand == null)
+
+            String bla = testraster.toString();
+
+            int i=0;
+            Rectangle testrectangle = testraster.getBounds();
+            int a = testrectangle.height;
+            int b = testrectangle.width;
+            System.out.println("b = " + b);
+            System.out.println("a = " + a);
+
+        // Create a buffer for reading a single scan line of the hi-band
+        float[] hiBandPixels = new float[w];
+
+        // Hi/Low-band sum and difference of the NDVI quotient
+        float sum, dif;
+
+        // NDVI value
+        float ndvi;
+
+        // NDVI value in the range 0 to 255
+        int ndviByte;
+
+        // For all scan lines in the product...
+        for (int y = 0; y < h; y++)
+        {
+            // Read low-band pixels for line y
+            lowBand.readPixels(0, y, w, 1, lowBandPixels, new PrintWriterProgressMonitor(System.out));
+
+            // Read hi-band pixels for line y
+            hiBand.readPixels(0, y, w, 1, hiBandPixels, new PrintWriterProgressMonitor(System.out));
+
+            // Compute NDVI for all x
+            for (int x = 0; x < w; x++)
             {
-                throw new IOException("hi-band 'radiance_10' not found");
-            }
+                dif = lowBandPixels[x] - hiBandPixels[x];
+                sum = lowBandPixels[x] + hiBandPixels[x];
 
-            // Print out, what we are going to do...
-            System.out.println("writing NDVI raw image file "
-                    + outputPath
-                    + " containing " + w + " x " + h
-                    + " pixels of type byte (value range 0-255)...");
-
-            // Create an output stream for the NDVI raw data
-            FileImageOutputStream outputStream = new FileImageOutputStream(new File(outputPath));
-
-            // Create a buffer for reading a single scan line of the low-band
-            float[] lowBandPixels = new float[w];
-
-            // Create a buffer for reading a single scan line of the hi-band
-            float[] hiBandPixels = new float[w];
-
-            // Hi/Low-band sum and difference of the NDVI quotient
-            float sum, dif;
-
-            // NDVI value
-            float ndvi;
-
-            // NDVI value in the range 0 to 255
-            int ndviByte;
-
-            // For all scan lines in the product...
-            for (int y = 0; y < h; y++)
-            {
-                // Read low-band pixels for line y
-                lowBand.readPixels(0, y, w, 1, lowBandPixels, new PrintWriterProgressMonitor(System.out));
-
-                // Read hi-band pixels for line y
-                hiBand.readPixels(0, y, w, 1, hiBandPixels, new PrintWriterProgressMonitor(System.out));
-
-                // Compute NDVI for all x
-                for (int x = 0; x < w; x++)
+                if (sum != 0.0F)
                 {
-                    dif = lowBandPixels[x] - hiBandPixels[x];
-                    sum = lowBandPixels[x] + hiBandPixels[x];
-
-                    if (sum != 0.0F)
-                    {
-                        ndvi = dif / sum;
-                    }
-
-                    else
-                        {
-                        ndvi = 0.0F;
-                    }
-
-                    if (ndvi < 0.0F)
-                    {
-                        ndvi = 0.0F;
-                    }
-
-                    else if (ndvi > 1.0F)
-                    {
-                        ndvi = 1.0F;
-                    }
-
-                    // Convert NDVI to integer in the range 0 to 255
-                    ndviByte = (int) (255 * ndvi);
-
-                    // write NDVI byte to raw image file
-                    outputStream.writeByte(ndviByte);
+                    ndvi = dif / sum;
                 }
+
+                else
+                {
+                    ndvi = 0.0F;
+                }
+
+                if (ndvi < 0.0F)
+                {
+                    ndvi = 0.0F;
+                }
+
+                else if (ndvi > 1.0F)
+                {
+                    ndvi = 1.0F;
+                }
+
+                // Convert NDVI to integer in the range 0 to 255
+                ndviByte = (int) (255 * ndvi);
+
+                // write NDVI byte to raw image file
+                outputStream.writeByte(ndviByte);
             }
-
-            // close raw image file
-            outputStream.close();
-
-            // Done!
-            System.out.println("OK");
         }
-    }
+
+        // close raw image file
+        outputStream.close();
+
+        // Done!
+        System.out.println("OK");
+    }*/
+}
