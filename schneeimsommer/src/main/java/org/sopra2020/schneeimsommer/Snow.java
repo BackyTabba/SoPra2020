c13eeeeccb2ec99781841bd861d00c579550ef42
jana2308walter <61973873+jana2308walter@users.noreply.github.com>
2020-03-12 17:23:33 +0100

    Update Snow.java

--- schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Snow.java
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Snow.java
@@ -3,11 +3,17 @@ package org.sopra2020.schneeimsommer;
 public class Snow
 {
     private float worthOfGrey;
+    private boolean isSnow;
     private int [] rgb = new int [3];
 
     public float getWorthOfGrey ()
     {
         return worthOfGrey;
+    }
+
+    public boolean getIsSnow ()
+    {
+        return isSnow;
     }
 
     public int[] getRgb ()
@@ -18,6 +24,11 @@ public class Snow
     public void setWorthOfGrey (float worthOfGrey)
     {
         this.worthOfGrey = worthOfGrey;
+    }
+
+    public void setSnow (boolean isSnow)
+    {
+        this.isSnow = isSnow;
     }
 
     public void setRgb (int [] rgb)
