378e9ff310043a1b19dcfe21ab9d0b2af0dbea38
jana2308walter <61973873+jana2308walter@users.noreply.github.com>
2020-03-12 16:45:40 +0100

    Update Geocordinates.java

--- schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Geocordinates.java
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Geocordinates.java
@@ -1,3 +1,5 @@
+package org.sopra2020.schneeimsommer;
+
 import org.esa.snap.core.datamodel.GeoCoding;
 import org.esa.snap.core.datamodel.GeoPos;
 import org.esa.snap.core.datamodel.PixelPos;
@@ -6,11 +8,26 @@ import java.awt.*;
 
 public class Geocordinates
 {
+    /*
+        @param product  The product with the Metadata of the Area of Interest (AOI)
+        @param geoPos   The geoposition of the AOI
+        @return PixelPos    The position of the coordinates on the picture
+        @see    DataManager
+     */
+
     public static PixelPos reference (Product product, GeoPos geoPos)
     {
         GeoCoding geoCoding = product.getSceneGeoCoding();
         return geoCoding.getPixelPos (geoPos, null);
     }
+
+
+    /*
+        @param product  The product with the Metadata of the Area of Interest (AOI)
+        @param geopos1   One of the angles of the rectangle for calculation of the rectangle
+        @param geopos2    The other one of the angles of the rectangle for calculation of the rectangle
+        @return Rectangle   The calculated rectangle
+    */
 
     public Rectangle createRectangle (Product product, GeoPos geopos1, GeoPos geopos2)
     {
@@ -19,17 +36,42 @@ public class Geocordinates
         return new Rectangle ((int) pixpos1.x, (int) pixpos1.y, (int) pixpos2.x - (int) pixpos1.x, (int) pixpos2.y - (int) pixpos1.y);
     }
 
+
+    /*
+        @param product  The product with the Metadata of the Area of Interest (AOI)
+        @param geopos1   One of the angles of the rectangle for calculation of the rectangle
+        @param width    The width of the rectangle
+        @param height   The height of the rectangle
+        @return Rectangle   The calculated rectangle
+    */
+
     public Rectangle createRectangle (Product product, GeoPos geopos1, int width, int height)
     {
         PixelPos pixpos1 = reference (product, geopos1);
         return new Rectangle ((int) pixpos1.x, (int) pixpos1.y, width, height);
     }
 
+
+    /*
+        @param pixpos1  One of the angles of the rectangle for calculation of the rectangle
+        @param pixpos2  The other one of the angles of the rectangle for calculation of the rectangle
+        @return Rectangle   The calculated rectangle
+    */
+
     public Rectangle createRectangle (PixelPos pixpos1, PixelPos pixpos2)
     {
         return new Rectangle ((int) pixpos1.x, (int) pixpos1.y, (int) pixpos2.x - (int) pixpos1.x, (int) pixpos2.y - (int) pixpos1.y);
     }
 
+
+    /*
+        @param product  The product with the Metadata of the Area of Interest (AOI)
+        @param pixpos1   One of the angles of the rectangle for calculation of the rectangle
+        @param width    The width of the rectangle
+        @param height   The height of the rectangle
+        @return Rectangle   The calculated rectangle
+    */
+    
     public Rectangle createRectangle (PixelPos pixpos1, int width, int height)
     {
         return new Rectangle ((int) pixpos1.x, (int) pixpos1.y, width, height);
