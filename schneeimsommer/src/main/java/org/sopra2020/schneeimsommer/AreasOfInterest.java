1a751102b74b30f698b5f19beb207047a49aa3c0
jana2308walter <61973873+jana2308walter@users.noreply.github.com>
2020-03-12 17:02:19 +0100

    Create AreasOfInterest.java

---
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/AreasOfInterest.java
@@ -0,0 +1,71 @@
+package org.sopra2020.schneeimsommer;
+
+import org.esa.snap.core.datamodel.GeoPos;
+
+public class AreasOfInterest
+{
+    private String name;
+    private GeoPos geoposSki1;
+    private GeoPos geoposSki2;
+    private GeoPos geoposRef1;
+    private GeoPos geoposRef2;
+
+    public AreasOfInterest (String name, GeoPos geoposSki1, GeoPos geoposSki2, GeoPos geoposRef1, GeoPos geoposRef2)
+    {
+        this.name = name;
+        this.geoposSki1 = geoposSki1;
+        this.geoposSki2 = geoposSki2;
+        this.geoposRef1 = geoposRef1;
+        this.geoposRef2 = geoposRef2;
+    }
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public GeoPos getGeoposSki1 ()
+    {
+        return geoposSki1;
+    }
+
+    public GeoPos getGeoposSki2 ()
+    {
+        return geoposSki2;
+    }
+
+    public GeoPos getGeoposRef1 ()
+    {
+        return geoposRef1;
+    }
+
+    public GeoPos getGeoposRef2 ()
+    {
+        return geoposRef2;
+    }
+
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+
+    public void setGeoposSki1 (GeoPos geoposSki1)
+    {
+        geoposSki1 = geoposSki1;
+    }
+
+    public void setGeoposSki2 (GeoPos geoposSki2)
+    {
+        geoposSki2 = geoposSki2;
+    }
+
+    public void setGeoposRef1 (GeoPos geoposRef1)
+    {
+        geoposRef1 = geoposRef1;
+    }
+
+    public void setGeoposRef2 (GeoPos geoposRef2)
+    {
+        geoposRef2 = geoposRef2;
+    }
+}
