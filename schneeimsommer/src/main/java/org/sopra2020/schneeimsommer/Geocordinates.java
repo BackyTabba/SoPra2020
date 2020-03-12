2ef2ffab08fbcde8daffff27d0982aec6c55ac77
A. Spork <a_spor03@wwu.de>
2020-03-12 10:52:38 +0100

    Update Geocordinates.java
    Added function to create rectangles from 2 Pixel/GeoPos or 1 Pixel/GeoPos with width and height

--- schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Geocordinates.java
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Geocordinates.java
@@ -5,6 +5,8 @@ import org.esa.snap.core.datamodel.GeoCoding;
 import org.esa.snap.core.datamodel.GeoPos;
 import org.esa.snap.core.datamodel.PixelPos;
 import org.esa.snap.core.datamodel.Product;
+
+import java.awt.*;
 import java.io.IOException;
 
 public class Geocordinates {
@@ -13,9 +15,29 @@ public class Geocordinates {
      * It shows how to convert from pixel coordinates to geo coordinates and back.
      * Note that you can import the output of this test program in BEAM application as transect profile.
      */
-    public static PixelPos reference(Product product, GeoPos geoPos)
-    {
+    public static PixelPos reference(@org.jetbrains.annotations.NotNull Product product, GeoPos geoPos) {
         GeoCoding geoCoding = product.getSceneGeoCoding();
         return geoCoding.getPixelPos(geoPos, null);
+    }
+    public static Rectangle createRectangle(Product product, GeoPos geopos1, GeoPos geopos2) {
+        PixelPos pixpos1 = reference(product, geopos1);
+        PixelPos pixpos2 = reference(product, geopos2);
+        Rectangle rec = new Rectangle((int) pixpos1.x, (int) pixpos1.y, (int) pixpos2.x - (int) pixpos1.x, (int) pixpos2.y - (int) pixpos1.y);
+        return rec;
+    }
+
+    public static Rectangle createRectangle(Product product, GeoPos geopos1, int width, int height) {
+        PixelPos pixpos1 = reference(product, geopos1);
+        Rectangle rec = new Rectangle((int) pixpos1.x, (int) pixpos1.y, width, height);
+        return rec;
+    }
+
+    public static Rectangle createRectangle(Product product, PixelPos pixpos1, PixelPos pixpos2) {
+        Rectangle rec = new Rectangle((int) pixpos1.x, (int) pixpos1.y, (int) pixpos2.x - (int) pixpos1.x, (int) pixpos2.y - (int) pixpos1.y);
+        return rec;
+    }
+    public static Rectangle createRectangle(Product product, PixelPos pixpos1, int width, int height) {
+        Rectangle rec = new Rectangle((int) pixpos1.x, (int) pixpos1.y, width, height);
+        return rec;
     }
 }
