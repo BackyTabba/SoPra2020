5a2513c0da69cffe35bef72160b710a24499338e
BackyTabba <l-lee12@web.de>
2020-03-13 11:29:31 +0100

    merge

--- schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/App.java
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/App.java
@@ -1,48 +1,39 @@
- package org.sopra2020.schneeimsommer;
+﻿ package org.sopra2020.schneeimsommer;
 
 import com.bc.ceres.glevel.MultiLevelImage;
 import com.google.common.primitives.Floats;
+import org.esa.s1tbx.io.imageio.ImageIOReader;
 import org.esa.snap.core.dataio.ProductIO;
-import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.datamodel.*;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import com.bc.ceres.core.PrintWriterProgressMonitor;
-import org.esa.snap.core.datamodel.Band;
-import org.esa.snap.core.datamodel.ProductData;
+import org.esa.snap.core.util.ProductUtils;
+import org.esa.snap.dataio.envisat.DataTypes;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
 import javax.imageio.stream.FileImageOutputStream;
+import javax.swing.*;
+import javax.xml.crypto.Data;
 import java.io.File;
+import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static jdk.nashorn.internal.objects.NativeMath.max;
 
 public class App
 {
-    /*public static void main (String[] args)
-    {
-        String inputPath = "",outputPath="";
-        if (args.length < 2)
-        {
-            System.out.println("parameter usage: <input-file> <output-file>");
-            return;
-        }
-
-        // Get arguments
-       // String inputPath = args[0];
-        //String outputPath = args[1];
-        try
-        {
-            // Pass arguments to actual program code
-            run(inputPath, outputPath);
-        }
-        catch (IOException e)
-        {
-            System.out.println("error: " + e.getMessage());
-        }
-    }
 
         Product product = null;
         System.out.println ("Baender:" + product.getBandNames());
@@ -227,6 +218,4 @@ public class App
         // Done!
         System.out.println("OK");
     }*/
-=======
->>>>>>> dev2
 }
