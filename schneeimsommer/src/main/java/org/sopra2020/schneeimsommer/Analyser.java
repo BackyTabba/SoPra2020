a23de327b2d6cf19dbac68d8cef32f3dd235e8a3
jana2308walter <61973873+jana2308walter@users.noreply.github.com>
2020-03-13 11:14:42 +0100

    Update Analyser.java

--- schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Analyser.java
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Analyser.java
@@ -3,66 +3,92 @@ package org.sopra2020.schneeimsommer;
 import java.sql.SQLSyntaxErrorException;
 
 /**
- * The Analyser gets up to four datasets as twodimensional array. It compares the measurements from sommer and winter
+ * The Analyser gets up to four datasets as twodimensional array. It compares the measurements from summer and winter
  * at two different areas to decide if a white area is winter and if its natural or a by human created product.
  */
-public class Analyser {
-    public final static float minSnow= 0f;  //smalest value for a snow area
-    public final static float maxSnow= 0f;  //biggest value for a snow area
+
+public class Analyser
+{
+    public final static float minSnow = 0f;  //smallest value for a snow area
+    public final static float maxSnow = 0f;  //biggest value for a snow area
 
     private float max;  //biggest measurement for a snow area
-    private float min;  //smalest measurement for a snow area
-    private int quantitySnow; //quantity of snow pixels
-    private float percentSnow; // Percent of snow in the measurement
-    private float[][] winterData;   //mesurments from winter
-    private float[][] summerData;   //mesuremets from summer
-    private float[][] winterDataRef;   //mesurments from winter as reference
-    private float[][] summerDataRef;   //mesurments from summer as reference
-    private Boolean[][] snowMask;   //mask of the area, which decides if its snow or just a white
-    private Boolean[][] snowMaskRef;   //mask of the reference area, which decides if its snow or just a white area
+    private float min;  //smallest measurement for a snow area
+    private int quantitySnow;   //quantity of snow pixels
+    private float percentSnow;  //percent of snow in the measurement
+    private float[][] winterData;   //measurements from winter
+    private float[][] summerData;   //measurements from summer
+    private float[][] winterDataRef;   //measurements from winter as reference
+    private float[][] summerDataRef;   //measurements from summer as reference
+    private Boolean[][] snowMask;   //mask of the area which decides if its snow or just a white area
+    private Boolean[][] snowMaskRef;    //mask of the reference area which decides if its snow or just a white area
+    private Snow[][] snow;  //final array for the output
+
 
     /**
-     *
-     * @param winterData Data set of the snowy area from winter
-     * @param summerData Data set of the snowy area from summer
+     * @param winterData    Data set of the snowy area from winter
+     * @param summerData    Data set of the snowy area from summer
      * @param winterDataRef Data set of the reference area from winter
      * @param summerDataRef Data set of the reference area from summer
      */
-    public Analyser(float[][] winterData, float[][] summerData, float[][] winterDataRef,float[][] summerDataRef) {
+
+    public Analyser (float[][] winterData, float[][] summerData, float[][] winterDataRef, float[][] summerDataRef)
+    {
         this.winterData = winterData;
         this.summerData = summerData;
         this.winterDataRef = winterDataRef;
         this.summerDataRef = summerDataRef;
 
-        snowMask = new Boolean[winterData.length][winterData[0].length]; //creat a reference if a white pixel is snow or something different
-        for (int i = 0; i < snowMask.length; i++) {
-            for (int j = 0; j < snowMask[0].length; j++) {
-                if (isSnow(winterData[i][j]) == true && isSnow(summerData[i][j]) == false) {
+        snowMask = new Boolean [winterData.length] [winterData[0].length];  //creates a reference if a white pixel is snow or something different
+        for (int i = 0; i < snowMask.length; i++)
+        {
+            for (int j = 0; j < snowMask[0].length; j++)
+            {
+                if (isSnow (winterData[i][j]) == true && isSnow (summerData[i][j]) == false)
+                {
                     snowMask[i][j] = true;
-                } else snowMask[i][j] = false;
+                }
+                else
+                {
+                    snowMask[i][j] = false;
+                }
             }
         }
-        snowMaskRef = new Boolean[winterDataRef.length][winterDataRef[0].length]; //creat a reference if a white pixel is snow or something different
-        for (int i = 0; i < snowMaskRef.length; i++) {
-            for (int j = 0; j < snowMask[0].length; j++) {
-                if (isSnow(winterData[i][j]) == true && isSnow(summerData[i][j]) == false) {
-                    snowMask[i][j] = true;
-                } else snowMask[i][j] = false;
+
+        snowMaskRef = new Boolean [winterDataRef.length] [winterDataRef[0].length]; //creates a reference if a white pixel is snow or something different
+        for (int i = 0; i < snowMaskRef.length; i++)
+        {
+            for (int j = 0; j < snowMask[0].length; j++)
+            {
+                if (isSnow (winterDataRef[i][j]) == true && isSnow (summerDataRef[i][j]) == false)
+                {
+                    snowMaskRef [i][j] = true;
+                }
+                else
+                {
+                    snowMaskRef [i][j] = false;
+                }
             }
 
         }
     }
 
+
     /**
-     *
-     * @param data
-     * @return the max value of the given array
+     * @param data   2-dimensional array to search the min value
+     * @return float    the max value of the given array
+     * @see Datamanager
      */
-    public float getMax(float[][] data){
-        float max=Float.MIN_VALUE;
-        for(int i=0; i < data.length; i++){
-            for (int j=0; j < data[0].length; j++) {
-                if(data[i][j] > max){
+
+    public float getMax (float[][] data)
+    {
+        float max = Float.MIN_VALUE;
+        for (int i = 0; i < data.length; i++)
+        {
+            for (int j = 0; j < data[0].length; j++)
+            {
+                if (data[i][j] > max)
+                {
                     max = data[i][j];
                 }
             }
@@ -70,17 +96,23 @@ public class Analyser {
         return max;
     }
 
+
     /**
-     *
-     * @param data 2-dimansional array to search the min value
-     * @return returns the min value of the given array
+     * @param data  2-dimensional array to search the min value
+     * @return float    returns the min value of the given array
+     * @see Datamanager
      */
-    public float minOfArray(float[][] data){
-        float min=Float.MAX_VALUE;
-        for(int i=0;i<data.length;i++) {
-            for (int j=0; j < data[0].length; j++) {
-                if(data[i][j]<min){
-                    min=data[i][j];
+
+    public float getMin (float[][] data)
+    {
+        float min = Float.MAX_VALUE;
+        for (int i = 0; i < data.length; i++)
+        {
+            for (int j = 0; j < data[0].length; j++)
+            {
+                if (data[i][j] < min)
+                {
+                    min = data[i][j];
                 }
             }
         }
@@ -89,15 +121,20 @@ public class Analyser {
 
 
     /**
-     *
-     * @param data - the data to be analysed
-     * @return returns the quantity of snow pixels
+     * @param data  the data to be analysed
+     * @return int  returns the quantity of snow pixels
+     * @see Datamanager
      */
-    public int getQuantitySnow(float[][] data){
-        int snow=0;
-        for(int i=0;i<data.length;i++) {
-            for (int j = 0; j < data[0].length; j++) {
-                if(data[i][j]>= minSnow && data[i][j] <=maxSnow){
+
+    public int getQuantitySnow (float[][] data)
+    {
+        int snow = 0;
+        for (int i = 0; i < data.length; i++)
+        {
+            for (int j = 0; j < data[0].length; j++)
+            {
+                if (data[i][j] >= minSnow && data[i][j] <= maxSnow)
+                {
                     snow++;
                 }
             }
@@ -107,40 +144,50 @@ public class Analyser {
 
 
     /**
-     *
-     * @param quantitySummerSnow
-     * @param quantityWinterSnow
-     * @return the quantity of snow pixels
+     * @param quantitySummerSnow    quantity of snow in summer
+     * @param quantityWinterSnow    quantity of snow in winter
+     * @return int  the quantity of snow pixels
      */
-    public int clean(int quantitySummerSnow, int quantityWinterSnow){
-        int realSnow= quantityWinterSnow - quantitySummerSnow;
-        if(realSnow<0){
-            System.err.println("Error: To much snow in Summer");
+
+    public int clean (int quantitySummerSnow, int quantityWinterSnow)
+    {
+        int realSnow = quantityWinterSnow - quantitySummerSnow;
+        if (realSnow < 0)
+        {
+            System.err.println ("Error: To much snow in summer");
         }
         return realSnow;
     }
 
+
     /**
-     * Makes the decision if a pixel is snow at all and if it is real or fake snow.
-     * @return a twodimensional snowarray with special color to show at the final results
+     * Makes the decision if a pixel is snow at all and if it is real or fake snow
+     * @return Snow[][]    snowarray with special color to show at the final results
      */
-    private Snow[][] colorSnow(){
 
-        Snow[][] snow = new Snow[winterData.length] [winterData[0].length];
+    public Snow[][] colorSnow()
+    {
+        snow = new Snow [winterData.length] [winterData[0].length];
+        for (int i = 0; i < snow.length; i++)
+        {
+            for (int j = 0; j < snow[0].length; j++)
+            {
+                snow[i][j].setIsSnowAtAll (snowMask[i][j]);
+                snow[i][j].setWorthOfGrey (winterData[i][j]);
+                //snow[i][j].setRgb( );             RGB Werte müssen noch gesetzt werden.
+                //Entweder über eine Umrechnung aus den Graustufen oder über die Farbe fürs Bild.
 
-        for (int i=0;i<snow.length;i++){
-            for (int j=0; j<snow[0].length; j++){
-                snow[i][j].SetIsSnowAtAll(snowMask[i][j]);
-                snow[i][j].setWorthOfGrey(winterData[i][j]);
-             //   snow[i][j].setRgb( );             RGB Werte müssen noch gesetzt werden.
-                //   Entweder über eine Umrechnung aus den Graustufen oder über die Farbe fürs Bild.
-                if (snow[i][j].getIsSnowAtAll()==false) {
-                    //Kein Schnee
-                    System.err.println("NoSnow at all");
-                }else if(clean(getQuantitySnow(winterDataRef),getQuantitySnow(summerDataRef))>=5){      // 5 = mistake acceptance
-                    snow[i][j].setRealSnow(true);// real snow
-                }else{
-                    snow[i][j].setRealSnow(false);// fake snow
+                if (snow[i][j].getIsSnowAtAll() == false)
+                {
+                    System.err.println("No snow at all");   //No snow
+                }
+                else if (clean (getQuantitySnow(winterDataRef), getQuantitySnow(summerDataRef)) >= 5)   // 5 mistakes are accepted
+                {
+                    snow[i][j].setRealSnow (true);   // real snow
+                }
+                else
+                {
+                    snow[i][j].setRealSnow (false);  // fake snow
                 }
             }
         }
@@ -148,14 +195,19 @@ public class Analyser {
     }
 
     /**
-     *
-     * @param f
-     * @return
+     * @param f an example measurement value
+     * @return boolean  returns true if f is between maxSnow and minSnow
      */
-    private boolean isSnow(float f){
-        if(f<=maxSnow && f>=minSnow){
-        return true;
-        }else return false;
-    }
 
+    private boolean isSnow (float f)
+    {
+        if (f <= maxSnow && f >= minSnow)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
