425725570c91cff50b5adc5104659b34f8871145
BackyTabba <l-lee12@web.de>
2020-03-13 12:36:29 +0100

    Create Main.java

---
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Main.java
@@ -0,0 +1,30 @@
+package org.sopra2020.schneeimsommer;
+
+import org.esa.snap.core.datamodel.GeoPos;
+
+public class Main {
+
+    public Main()
+    {
+
+    }
+    public static void main(String args[])
+    {
+        /*
+        DataManager dataManagerWinter = new DataManager("<<InputPath>>");
+        DataManager dataManagerSummer = new DataManager("<<InputPath>>");
+        Geocoordinates geoWinter = new Geocoordinates(dataManager.getProduct());
+        Geocoordinates geoSummer = new Geocoordinates(dataManager.getProduct());
+        //AreasOfInterest aoiWinter = new AreasOfInterest("Zillertal",new GeoPos(,),new GeoPos(,),new GeoPos(,),new GeoPos(,))  // Die Koordinaten müssen noch geklärt werden
+        //AreasOfInterest aoiSummer = new AreasOfInterest("Zillertal",new GeoPos(,),new GeoPos(,),new GeoPos(,),new GeoPos(,))  // Die Koordinaten müssen noch geklärt werden
+        Analyser analyser = new Analyser();
+        dataManagerWinter.      extractData(dataManager.getProduct(),geo.createRectangle(   aoiWinter.  geoposSki1, aoiWinter.  geoposPos2))           //Daten Ski winter
+        dataManagerWinterRef.   extractData(dataManager.getProduct(),geo.createRectangle(   aoiWinter.  geoposRef1, aoiWinter.  geoposRef2))           //Daten Golf winter
+        dataManagerWinter.      extractData(dataManager.getProduct(),geo.createRectangle(   aoiSummer.  geoposSki1, aoiSummer.  geoposPos2))           //Daten Ski summer
+        dataManagerWimmerRef.   extractData(dataManager.getProduct(),geo.createRectangle(   aoiSummer.  geoposRef1, aoiSummer.  geoposRef2))           //Daten Golf summer
+
+        */
+
+    }
+
+}
