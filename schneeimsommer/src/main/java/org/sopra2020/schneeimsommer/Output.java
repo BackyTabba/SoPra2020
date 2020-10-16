8e6d1170e529ea86000099c0673e13298d93d19a
speckij <speckij@gmail.com>
2020-10-16 10:33:49 +0200

    remove unused imports

--- schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Output.java
+++ schneeimsommer/src/main/java/org/sopra2020/schneeimsommer/Output.java
@@ -1,36 +1,8 @@
 package org.sopra2020.schneeimsommer;
 
-import com.bc.ceres.glevel.MultiLevelImage;
-import com.google.common.primitives.Floats;
-import org.esa.s1tbx.io.imageio.ImageIOReader;
-import org.esa.snap.core.dataio.ProductIO;
-import org.esa.snap.core.datamodel.*;
-
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.Raster;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import com.bc.ceres.core.PrintWriterProgressMonitor;
-import org.esa.snap.core.util.ProductUtils;
-import org.esa.snap.dataio.envisat.DataTypes;
-
-import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
-import javax.imageio.ImageWriteParam;
-import javax.imageio.ImageWriter;
-import javax.imageio.stream.FileImageOutputStream;
-import javax.swing.*;
-import javax.xml.crypto.Data;
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static jdk.nashorn.internal.objects.NativeMath.max;
 
 // The class for creating the output as a picture
 
